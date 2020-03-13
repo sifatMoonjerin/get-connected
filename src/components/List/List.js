faa2edbdb88caa1a43c5438f0f98c9ae6c006c9f
sifatMoonjerin <moonjerin.sifat@gmail.com>
2020-03-13 19:57:31 +0600

    connect btn done

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,13 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 import User from '../User/User';
 import './List.css';
+import fakeUsers from '../../fakeUsers/fakeUsers';
 
-const List = (props) => {
+const List = () => {
+    const newUsers = fakeUsers.map(user => {
+        user.isConnected = false;
+        return user;
+    });
+
+    const [users, setUsers] = useState(newUsers);
+    const [count, setCount] = useState([]);
+
+    const handleConnectBtn = (person, status) => {
+        if(status === "connect"){
+            setCount([...count, person])
+        } else{
+            setCount(count.filter(user => user.id !== person.id))
+        } 
+        const updatedUsers = users.map(user => {
+            if(user.id === person.id){
+                user = {...user, "isConnected":!user.isConnected}
+            }
+            return user;
+        })
+        setUsers(updatedUsers)
+    }
+
+    
+
     return (
         <div className="user-list">
-            {props.users.map(user => {
-                return <User user={user}></User>
+            {users.map(user => {
+                return <User user={user} handleConnectBtn={handleConnectBtn}></User>
             })}
+            {console.log(count)}
         </div>
     );
 };
--- src/components/User/User.js
+++ src/components/User/User.js
@@ -1,36 +1,47 @@
 import React from 'react';
 import './User.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faUserFriends, faPhone, faEnvelope, faDesktop, faDollarSign } from '@fortawesome/free-solid-svg-icons';
+import { faUserFriends, faPhone, faEnvelope, faDesktop, faDonate, faUserMinus } from '@fortawesome/free-solid-svg-icons';
 
 const User = (props) => {
+    const updateButton = () => {
+        if(props.user.isConnected){
+            return <button onClick={() => props.handleConnectBtn(props.user,'disconnect')} className="disconnect">
+            <FontAwesomeIcon icon={faUserMinus} /> disconnect
+        </button>
+        } else{
+            return <button onClick={() => props.handleConnectBtn(props.user,'connect')} className="connect">
+            <FontAwesomeIcon icon={faUserFriends} /> connect
+        </button>
+        }
+    }
+
     return (
         
-            <div className="container">
-            <div className="img-container">
-                <img src={props.user.background} alt="" className="banner-img"></img>
-                <img src={props.user.image} alt="" className="profile-img"></img>
+        <div className="container">
+        <div className="img-container">
+            <img src={props.user.background} alt="" className="banner-img"></img>
+            <img src={props.user.image} alt="" className="profile-img"></img>
+        </div>
+
+
+        <div className="content">
+            <div className="title">
+            <p>{props.user.name}</p>
+            <span>@{props.user.username}</span>
             </div>
 
-
-            <div className="content">
-                <div className="title">
-                <p>{props.user.name}</p>
-                <span>@{props.user.username}</span>
-                </div>
-
-                <div className="info">
-                    <p><FontAwesomeIcon icon={faDollarSign}  /> {props.user.salary}</p>
-                    <p><FontAwesomeIcon icon={faPhone} /> {props.user.phone}</p>
-                    <p><FontAwesomeIcon icon={faEnvelope} /> {props.user.email}</p>
-                    <p><FontAwesomeIcon icon={faDesktop}  /> {props.user.website}</p>
-                </div>
-
-                <button className="connect">
-                    <FontAwesomeIcon icon={faUserFriends} /> connect
-                </button>
+            <div className="info">
+                <p><FontAwesomeIcon icon={faDonate}  /> {props.user.salary}</p>
+                <p><FontAwesomeIcon icon={faPhone} /> {props.user.phone}</p>
+                <p><FontAwesomeIcon icon={faEnvelope} /> {props.user.email}</p>
+                <p><FontAwesomeIcon icon={faDesktop}  /> {props.user.website}</p>
             </div>
-            </div>
+
+            {updateButton()}
+
+        </div>
+        </div>
         
     );
 };
